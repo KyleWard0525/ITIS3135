0b00b9895f2909f2311192632df9d85cb6c0857e
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-10-27 17:21:54 -0400

    Removed preload()

--- root/scripts/root.js
+++ root/scripts/root.js
@@ -1,21 +1,16 @@
 /**
  * This file contains functions for supporting the root homepage
  */
-
-// Perform pre-load operations
-function preload()
-{
-    const bgImg = new Image().src= "images/space.jpg";
-}
-
 // Perform all necessary functions at load-time
 function onLoad()
 {
     let mainFadeDelay = 3030;
 
+    $("main").hide();
+
     $(window).ready(function() {
         // Fade in content
-        $("main").hide();
+        
         
         $("main").fadeIn(mainFadeDelay); 
     });
